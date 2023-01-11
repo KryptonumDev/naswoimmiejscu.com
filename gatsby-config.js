5008545eb445564bee5cdbf314f657721a5f15ea
CodeYouLike <strzyzewski.albertms@gmail.com>
2023-01-11 20:26:15 +0100

    Implementacja fontów, podstawowy komponent tekstu

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,22 +1,35 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
+
+require("dotenv").config({
+  path: ".env",
+});
+
 module.exports = {
   siteMetadata: {
     title: `naswoimmiejscu`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-image", "gatsby-plugin-sitemap", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: `gatsby-source-wordpress`,
-    options: {
-      url: `https://wp-naswoimmiejscu.headlesshub.com/graphql`,
-    },
-  }, {
-      resolve: 'gatsby-source-filesystem',
+  plugins: [
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: `gatsby-source-wordpress`,
       options: {
-        "name": "images",
-        "path": "./src/images/"
+        url: `https://wp-naswoimmiejscu.headlesshub.com/graphql`,
       },
-      __key: "images"
-    }]
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
 };
---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,58 @@
+import { createGlobalStyle } from "styled-components";
+import "./fonts.css";
+import "./normalize.css";
+
+const GlobalStyle = createGlobalStyle`
+    html{
+        --normalGreen: #0BC76D;
+        --hoverGreen: #0FB264;
+        --activeGreen: #0D9E59;
+        --focusGreen: #0FB364;
+        --disabledGreen: #C3CEC9;
+        --buttonWhiteNormal: #EDEDED;
+        --buttonBrownHover: #745239;
+        --buttonBrownActive: #593922;
+        --buttonBrownFocus: #745239;
+        --normalBlur: #FFFBEF;
+        --nromalBrown: #6C5F4D;
+        --normalGrey: #606164;
+        --normalWhite: #ffffff; 
+    }
+
+    body {
+      overflow-x: hidden;
+      width: 100%;
+    }
+    
+    *{
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+      -webkit-tap-highlight-color: transparent;
+      font-variant-numeric: oldstyle-nums;
+    }
+
+    *:focus-visible {
+      outline-width: 1px;
+      outline-style: solid;
+      outline-color: #da9610;
+      outline-offset: 4px;
+    }
+
+    *:focus {
+      outline: none
+    }
+
+    p{
+      font-variant-numeric: oldstyle-nums;
+    }
+
+    *::before,
+    *::after{
+        box-sizing: inherit;
+        margin: 0;
+        padding: 0;
+    }
+`;
+
+export default GlobalStyle;
